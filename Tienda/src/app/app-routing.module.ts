ce6c815ab3a9dbdf1f66d7a32a5146b62c818892
juankcl <esjuankarlos1@gmail.com>
2020-05-28 20:05:08 -0500

    Agregar paginas 3

--- Tienda/src/app/app-routing.module.ts
+++ Tienda/src/app/app-routing.module.ts
@@ -10,6 +10,18 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'buscar',
+    loadChildren: () => import('./pages/buscar/buscar.module').then( m => m.BuscarPageModule)
+  },
+  {
+    path: 'cuenta',
+    loadChildren: () => import('./pages/cuenta/cuenta.module').then( m => m.CuentaPageModule)
   }
 ];
 
